abccc18640c67b2506f1e2488b9703dedbaaa5c5
4xel-C <axel-cano@live.fr>
2026-06-28 12:39:33 +0200

    Submitting problem 766: toeplitz matrices

--- 766_toeplitz_matrix.py
+++ 766_toeplitz_matrix.py
@@ -19,37 +19,21 @@ In each diagonal all elements are the same, so the answer is True.
 from typing import List
 
 
-# TODO: take into consideration rectangular matrix
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        first_line = matrix[0]
-        first_column = [matrix[0][0]]
-
         if len(matrix[0]) <= 1:
             return True
 
-        for row in range(1, len(matrix)):
-            # Add the first element of the row in the vectical line in first position for comparison
-            first_column.insert(0, matrix[row][0])
-
-            print(f"===Iteration {row}===")
-            print("first line: ", first_line[:-row])
-            print("compared with: ", matrix[row][row:])
-            print("------")
-            print("first column: ", first_column)
-            print("compared with: ", matrix[row][: row + 1])
-
-            if (
-                matrix[row][row:] != first_line[:-row]
-                or matrix[row][: row + 1] != first_column
-            ):
-                return False
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
 
         return True
 
 
 if __name__ == "__main__":
-    matrix = [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]]
+    matrix = [[97, 97], [80, 97], [10, 80]]
     solver = Solution()
 
     print(solver.isToeplitzMatrix(matrix))
