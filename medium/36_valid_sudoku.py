a5c6388cd2cba51c2121ec55c24c802bd07a5812
4xel-C <axel-cano@live.fr>
2026-08-03 13:54:50 +0200

    Adding problem 36

---
+++ medium/36_valid_sudoku.py
@@ -0,0 +1,108 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+
+Example 1:
+
+
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+Example 2:
+
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # compute the sets for column and the squares, to keep track of the found number and quickly check for duplicates
+        col_sets = [set() for _ in range(len(board))]
+
+        square_sets = [
+            [set() for _ in range(len(board[0]) // 3)] for _ in range(len(board) // 3)
+        ]
+
+        # iteration on all row and columns
+        for row in range(len(board)):
+            row_set = set()
+
+            for col in range(len(board[0])):
+                # ignore if no numbers
+                if board[row][col] == ".":
+                    continue
+
+                value = board[row][col]
+
+                # Check the row
+                if value in row_set:
+                    return False
+
+                # Check the col
+                if value in col_sets[col]:
+                    return False
+
+                # check the square
+                if value in square_sets[row // 3][col // 3]:
+                    return False
+
+                # Add the value to the concerned sets
+                row_set.add(value)
+                col_sets[col].add(value)
+                square_sets[row // 3][col // 3].add(value)
+
+        return True
+
+
+if __name__ == "__main__":
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    ]
+
+    solver = Solution()
+
+    print(solver.isValidSUdoku(board))
