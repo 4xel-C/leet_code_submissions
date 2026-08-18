1c30ab8302cf55e18376dd9aa06e8c38ff28bfc6
4xel-C <axel-cano@live.fr>
2026-08-18 09:50:41 +0200

    Adding commentary for further solving

--- medium/45_jump_game.py
+++ medium/45_jump_game.py
@@ -31,7 +31,13 @@ from typing import List
 
 
 class Solution:
-    def jump(self, nums: List[int]) -> int:
+    def jump(self, nmus: List[int]) -> int: ...
+
+    """Try to build a graph ? DFS / BFS ? Is Dijkstraa algorithm usable ?"""
+
+    def jump_recursion(self, nums: List[int]) -> int:
+        """Note: This solution is not optimal and will result in a time limit exceeded error for large inputs."""
+
         def _recursion(nums, start=0, jump_count=0) -> float:
             if start == len(nums) - 1:
                 return jump_count
@@ -52,5 +58,45 @@ class Solution:
 
 if __name__ == "__main__":
     solver = Solution()
-    nums = [2, 3, 1, 1, 4]
+    nums = [
+        5,
+        6,
+        4,
+        4,
+        6,
+        9,
+        4,
+        4,
+        7,
+        4,
+        4,
+        8,
+        2,
+        6,
+        8,
+        1,
+        5,
+        9,
+        6,
+        5,
+        2,
+        7,
+        9,
+        7,
+        9,
+        6,
+        9,
+        4,
+        1,
+        6,
+        8,
+        8,
+        4,
+        4,
+        2,
+        0,
+        3,
+        8,
+        5,
+    ]
     print(solver.jump(nums))
